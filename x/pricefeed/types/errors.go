cef619f2f60bb7cfaaea61861ce83eb565809eda
satawatnack <s.nack1919@gmail.com>
2023-02-14 14:28:43 +0700

    change error code

--- x/pricefeed/types/errors.go
+++ x/pricefeed/types/errors.go
@@ -8,9 +8,9 @@ import (
 
 // x/pricefeed module sentinel errors
 var (
-	ErrSymbolRequestNotFound  = sdkerrors.Register(ModuleName, 1, "symbol request not found")
-	ErrSymbolRequestsNotFound = sdkerrors.Register(ModuleName, 2, "symbol requests not found")
-	ErrPriceNotFound          = sdkerrors.Register(ModuleName, 3, "price not found")
+	ErrSymbolRequestNotFound  = sdkerrors.Register(ModuleName, 2, "symbol request not found")
+	ErrSymbolRequestsNotFound = sdkerrors.Register(ModuleName, 3, "symbol requests not found")
+	ErrPriceNotFound          = sdkerrors.Register(ModuleName, 4, "price not found")
 
 	ErrSample               = sdkerrors.Register(ModuleName, 1100, "sample error")
 	ErrInvalidPacketTimeout = sdkerrors.Register(ModuleName, 1500, "invalid packet timeout")
