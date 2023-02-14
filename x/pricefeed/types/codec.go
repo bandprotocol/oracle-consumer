b7ae4cf8d1ed06c9a93d79eaffb64136d61c77dc
satawatnack <s.nack1919@gmail.com>
2023-02-14 14:22:06 +0700

    add starport scaffolding back

--- x/pricefeed/types/codec.go
+++ x/pricefeed/types/codec.go
@@ -16,6 +16,7 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 		&UpdateSymbolRequestProposal{},
 	)
 
+	// this line is used by starport scaffolding # 3
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
 
--- x/pricefeed/types/events_ibc.go
+++ x/pricefeed/types/events_ibc.go
@@ -3,7 +3,7 @@ package types
 // IBC events
 const (
 	EventTypeTimeout = "timeout"
-
+	// this line is used by starport scaffolding # ibc/packet/event
 	AttributeKeyAckSuccess = "success"
 	AttributeKeyAck        = "acknowledgement"
 	AttributeKeyAckError   = "error"
