898f9525e97f3d349275a28554ea76aaae383df0
satawatnack <s.nack1919@gmail.com>
2023-02-17 15:50:18 +0700

    remove ts-client

--- ts-client/consumer.oracleconsumer/registry.ts
+++
@@ -1,6 +0,0 @@
-import { GeneratedType } from '@cosmjs/proto-signing'
-import { MsgSetRequestInterval } from './types/consumer/pricefeed/tx'
-
-const msgTypes: Array<[string, GeneratedType]> = [['/consumer.pricefeed.MsgSetRequestInterval', MsgSetRequestInterval]]
-
-export { msgTypes }
--- ts-client/consumer.oracleconsumer/types.ts
+++
@@ -1,5 +0,0 @@
-import { PriceFeedPacketData } from './types/consumer/pricefeed/packet'
-import { NoData } from './types/consumer/pricefeed/packet'
-import { Params } from './types/consumer/pricefeed/params'
-
-export { PriceFeedPacketData, NoData, Params }
