cdaaf654adf6104e95f5443e2de41519d1d9ca2e
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2023-04-18 23:56:48 +0700

    remove unused code

--- x/pricefeed/pricefeed_test.go
+++ x/pricefeed/pricefeed_test.go
@@ -20,14 +20,12 @@ type PricefeedTestSuite struct {
 	// testing chains used for convenience and readability
 	chainA *ibctesting.TestChain
 	chainB *ibctesting.TestChain
-	chainC *ibctesting.TestChain
 }
 
 func (suite *PricefeedTestSuite) SetupTest() {
 	suite.coordinator = ibctesting.NewCoordinator(suite.T(), 3)
 	suite.chainA = suite.coordinator.GetChain(ibctesting.GetChainID(1))
 	suite.chainB = suite.coordinator.GetChain(ibctesting.GetChainID(2))
-	suite.chainC = suite.coordinator.GetChain(ibctesting.GetChainID(3))
 
 	// set localhost client
 	revision := clienttypes.ParseChainID(suite.chainA.GetContext().ChainID())
