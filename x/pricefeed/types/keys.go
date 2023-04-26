e53195ca44f7badcb828abf856ec0316c53079a2
satawatnack <s.nack1919@gmail.com>
2023-04-26 17:12:10 +0700

    change port key name

--- x/pricefeed/types/keys.go
+++ x/pricefeed/types/keys.go
@@ -21,16 +21,12 @@ var (
 	GlobalStoreKeyPrefix = []byte{0x00}
 
 	// PortKey defines the key to store the port ID in store
-	PortKey = KeyPrefix("pricefeed-port-")
+	PortKey = []byte{0x01}
 
-	SymbolRequestStoreKeyPrefix = []byte{0x01}
+	SymbolRequestStoreKeyPrefix = []byte{0x02}
 
-	PriceStoreKeyPrefix = []byte{0x02}
+	PriceStoreKeyPrefix = []byte{0x03}
 )
-
-func KeyPrefix(p string) []byte {
-	return []byte(p)
-}
 
 func SymbolRequestStoreKey(symbol string) []byte {
 	return append(SymbolRequestStoreKeyPrefix, []byte(symbol)...)
